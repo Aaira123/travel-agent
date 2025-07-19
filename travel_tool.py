44141ac7d9fd835f8ee8e21efda5ff2d70dd8efc
sana arshad <160604463+Aaira123@users.noreply.github.com>
2025-07-19 13:05:18 +0500

    Add files via upload
    travel-agent has completed

---
+++ travel_tool.py
@@ -0,0 +1,11 @@
+from  agents import function_tool
+
+@function_tool
+def get_flights(destination: str) -> str:
+    
+    return f"flights found to {destination}: PKR 45,000 - PKR 70,000."
+
+@function_tool
+def suggest_hotels(destination: str) -> str:
+    
+    return f"Hotels in {destination}: Pearl Continental, Marriott, Local Guest Houses."
